441f41bccde75fa2a6e6b72c43e27eef1458b7bb
egbuniwecemeka <egbuniwecemeka.com>
2024-10-10 18:40:33 +0100

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    A type-annotated function that adds two float arguments
+
+    Args:
+        a: The first number
+        b: The second number
+
+    Return:
+        float: The sum of a and b
+    """
+    return a + b
