2b772bec0df30d7e31ff9092faecc0d8be536e0a
egbuniwecemeka <egbuniwecemeka.com>
2024-10-10 21:28:49 +0100

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" A python script on basic annotations """
+
+
+def floor(n: float) -> int:
+    return n
---
+++ 0x00-python_variable_annotations/2-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from math import floor
+
+
+floor = __import__('2-floor').floor
+
+ans = floor(3.14)
+print(ans == floor(3.14))
+print(ans.__annotations__)
+print(f" returns {ans}, which is a {type(ans)}")
