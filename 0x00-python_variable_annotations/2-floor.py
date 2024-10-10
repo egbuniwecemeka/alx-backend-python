535cef965801e68a24cd8198588f9e092e0fc536
egbuniwecemeka <egbuniwecemeka.com>
2024-10-10 22:06:39 +0100

    Basic annotations - floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ A python script on basic annotations """
 
+from math import floor
 
 def floor(n: float) -> int:
-    return n
+    """ Returns te floor of the float number n """
+    return floor(n)
