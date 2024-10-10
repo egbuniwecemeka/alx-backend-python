40e7a1525f158437bf08acfc6cc70ea004fdc48e
egbuniwecemeka <egbuniwecemeka.com>
2024-10-10 21:39:24 +0100

    Basic annotations - floor

--- 0x00-python_variable_annotations/2-main.py
+++ 0x00-python_variable_annotations/2-main.py
@@ -8,4 +8,4 @@ floor = __import__('2-floor').floor
 ans = floor(3.14)
 print(ans == floor(3.14))
 print(ans.__annotations__)
-print(f" returns {ans}, which is a {type(ans)}")
+print(f" floor(3.14)    returns {ans}, which is a {type(ans)}")
