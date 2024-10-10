12b70674e06a6d3b276882ee8b31f3e13a240fba
egbuniwecemeka <egbuniwecemeka.com>
2024-10-10 21:12:06 +0100

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" A python script exhibiting basic strings annotation """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    A type-annotated function taking two argument and returns a string
+    """
+    return str1 + str2
