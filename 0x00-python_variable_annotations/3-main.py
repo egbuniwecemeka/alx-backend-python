479f572d356d857c739ef6433e55a4f5d968af80
egbuniwecemeka <egbuniwecemeka.com>
2024-10-10 23:12:23 +0100

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+to_str = __import__('3-to_str').to_str
+
+pi_str = to_str(3.14)
+
+print(pi_str == str(3.14))
+print(to_str.__annotations__)
+print(f" floor(3.14) returns {pi_str}, which is a {type()}")
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" A python script on basic float to string annotations """
+
+
+def to_str(n: float) -> str:
+    """ Returns the string of the float n"""
+    return n
