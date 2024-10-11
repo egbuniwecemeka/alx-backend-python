96aa1edf02f408b8d2bf39ac77a746192a0c68d9
egbuniwecemeka <egbuniwecemeka.com>
2024-10-11 01:24:05 +0100

    Complex types - list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A python script taking a list of floats and returns their sums (float)"""
+
+
+def sum_list(input_list: list[float]) -> list[float]:
+    """Returns a list of float arguments """
+    return list(input_list)
