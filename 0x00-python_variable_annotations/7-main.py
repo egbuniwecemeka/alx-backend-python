dfc9607f88c984a512d6c7ecc5da69803cbb4a65
egbuniwecemeka <egbuniwecemeka.com>
2024-10-12 22:37:47 +0100

    Complex types - string and int/float to tuple

---
+++ 0x00-python_variable_annotations/7-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+to_kv = __import__('7_to_kv').to_kv
+
+value = to_kv("emma", 3.14)
+print(value)
---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+from typing import Union
+
+def to_kv(k: str, v: Union[int, float]) -> tuple:
+    return (k, float(v) ** 2)
