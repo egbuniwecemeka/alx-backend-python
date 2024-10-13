22860168170c90bd108b3caf728b89dee862a2d0
egbuniwecemeka <egbuniwecemeka.com>
2024-10-13 02:09:24 +0100

    Complex types - string and int/float to tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,4 +4,5 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ Returns a tuple, of a string and an int or float"""
     return (k, float(v) ** 2)
