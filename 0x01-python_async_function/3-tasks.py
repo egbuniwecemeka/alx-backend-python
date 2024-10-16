8980bd43442e0c1e7195118cf208ffa0024929f8
egbuniwecemeka <egbuniwecemeka.com>
2024-10-16 11:41:39 +0100

    Tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" A python script that takes an integer and rreturns an asyncio.Task """
+
+import asyncio
+from typing import Task
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Task:
+    """ 
+    takes an integer and returns a asyncio.Task
+
+    Args:
+    max_delay (int): Max delay for task
+
+    Returns:
+    Returns a asyncio.Task
+    """
+    return asyncio.create_task(wait_random(max_delay))
