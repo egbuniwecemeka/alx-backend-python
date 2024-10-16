6fbcacdaad5fe6db405f9742c20cdf90daf3c4c5
egbuniwecemeka <egbuniwecemeka.com>
2024-10-16 01:50:41 +0100

    Measure the runtime

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -4,10 +4,10 @@
 import time
 import asyncio
 
-time_n = __import__('1-concurrent_coroutines').time_n
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Asynchronous coroutine that measures the execution runtime for 
     wait_n(n, max_delay)
@@ -23,7 +23,7 @@ async def measure_time(n: int, max_delay: int) -> float:
     start_time = time.time()
 
     # synchronously run wait_n
-    asyncio.run(time_n(n, max_delay))
+    asyncio.run(wait_n(n, max_delay))
 
     # Record end time
     end_time = time.time()
