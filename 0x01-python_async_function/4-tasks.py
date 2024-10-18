c0b565f9810de855636a7c34a4d70c06d09b5add
egbuniwecemeka <egbuniwecemeka.com>
2024-10-18 21:26:10 +0100

    Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" A python script exhibiting concurrent/asynchronous operations """
+
+import asyncio
+from typing import List
+from bisect import insort
+
+wait_random = __import__()
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ 
+    Spawns task_wait_n n times with the specified delay time
+    Also, returns a list of all delay values as floats
+
+    Args:
+    n (int): Number ot times to spawn task_wait_n
+    max_delay: Maximum delay time for task_wait_n
+
+    Return:
+    List of all delay values (floats)
+    """
+    spawn = [task_wait_n(max_delay) for _ in range(n)]
+
+    # run spqwned tasks concurrently and collect their values
+    delays = await asyncio.gather(*spawn)
+
+    # Manual sorting without using sort()
+    sorted_task = []
+    for delay in delays:
+        insort(sorted_task, delay)
+
+    return sorted_task
