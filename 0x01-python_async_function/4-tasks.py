8971be00ec8d520b162557b2e1a069cf2bb40dc2
egbuniwecemeka <egbuniwecemeka.com>
2024-10-18 21:34:03 +0100

    Tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,7 +5,7 @@ import asyncio
 from typing import List
 from bisect import insort
 
-wait_random = __import__()
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
