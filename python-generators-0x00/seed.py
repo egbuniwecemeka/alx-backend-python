e98aa8d01ecd3c88311abac3d3d3480195b51575
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-09-16 06:57:34 +0100

    Getting started with python generators

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -1,68 +1,76 @@
-#!/usr/bin/python3
-
-""" A python script that connects to a DB and populates it with data"""
-
-from sqlalchemy import  create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DECIMAL
-from sqlalchemy.dialects.mysql import CHAR
-from sqlalchemy.orm import sessionmaker
-from uuid import uuid4
+from mysql import connector
+from mysql.connector import Error
 from sys import argv
+from csv import DictReader
+from uuid import uuid4
 
-
-
-Base = declarative_base()
-
-# echo is a shortcut setting up SQLAlchemy logging (python's 'logging module')
-# it (echo) generates all SQL output. Set it to False, for less output
-# sqlite dialect interprets to the pythons sqlite3 module.
-engine = create_engine(f'mysql+mysqlconnector://{argv[1]}:{argv[2]}@localhost/ALX_prodev', echo=True)
-
-# return value of create_engine is an Engine instance
-
-
-# connection successful
-# Database ALX_prodev is present
-
-
-
-class User(Base):
-    """ Base users class """
-    __tablename__ = 'user_data'
-
-    # During actual class constructor, Declarative replaces Column with
-    # python accessors know as descriptors. Process is know as instrumentation.
-    # instrumentation makes it possible to refer to tabless in SQL context.
-    user_id = Column(CHAR(36), primary_key=True, default=lambda: str(uuid4()), index=True)
-    name = Column(String(255), nullable=False)
-    email = Column(String(255), nullable=False)
-    age = Column(DECIMAL(10, 2), nullable= False)
-
-    def __repr__(self):
-        return f"<User(user_id='{self.user_id}', name='{self.name}', email='{self.email}', age='{self.age}')>"
-    
 def connect_db():
-    """ Connects to the mysql server """
-    Session = sessionmaker(bind=engine)
-
-    # Whenever there is need for a converstation. a Session is instantiated
-    # session = Session()
+    """ Connects to a DB server """
+    try:
+        conn = connector.connect(
+            host = 'localhost',
+            user = argv[1],
+            password = argv[2]
+        )
+        return conn
+    except Error as e:
+        print(f'Error {e}')
+        return None
 
 def create_database(connection):
-    """ Creates a database ALX_prodev if it does not exist """
-    pass
+    """ Creates a database ALX_prodev if it doesn't exist """
+    cursor = connection.cursor()
+    cursor.execute('CREATE DATABASE IF NOT EXISTS ALX_prodev;')
+    cursor.close()
+
+def connect_to_prodev():
+    """ Connect to ALX_prodev DB """
+    try:
+        conn = connector.connect(
+            host = 'localhost',
+            user = argv[1],
+            password = argv[2],
+            database = 'ALX_prodev'
+        )
+        return conn
+    except Error as e:
+        print(f"Error {e}")
+        return None
 
 def create_table(connection):
-    """ Creates a table user_data if it does not exist,
-        with required fields
-    """
-    pass
+    """ Creates a table user_data if not existing """
+    cursor = connection.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS user_data (
+            user_id CHAR(36) PRIMARY KEY NOT NULL DEFAULT (UUID()),
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            age DECIMAL(10, 2) NOT NULL,
+            INDEX(user_id)
+        )
+        """
+    )
+    cursor.close()
 
 def insert_data(connection, data):
-    """ insert data in the database if it does not exist """
-    pass
-  
-# Table user_data created successfully
-# Inspect the table attribute(schema)
-print(User.__table__)
+    """ Inserts data into user table from a csv file """
+    cursor = connection.cursor()
+    with open(data, newline='') as f:
+        reader = DictReader(f)
+        for row in reader:
+            # Check if email exists to avoid duplcates
+            cursor.execute("SELECT email FROM user_data WHERE email = %s", (row["email"],))
+            if cursor.fetchone():
+                continue
+            query = """
+                        INSERT INTO user_data (user_id, name, email, age)
+                        VALUES (%s, %s, %s, %s);
+                    """
+            user_data = (str(uuid4()), row["name"], row["email"], float(row["age"]))
+            try:
+                cursor.execute(query, user_data)
+            except Error as e:
+                print(f"Insert values: {e}")
+                connection.rollback()
+    connection.commit()
+    cursor.close()
