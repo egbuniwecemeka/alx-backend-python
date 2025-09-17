364e67d7fd3e9ea395bb185ae8faf0de61d16775
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-09-17 18:56:34 +0100

    Batch processing Large Data

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,21 @@
+""" A generator script that fetches and processes data in batches from the users DB """
+
+from seed import connect_to_prodev
+
+connection = connect_to_prodev()
+
+def stream_users_in_batches(batch_size):
+    """ Fetches rows in batches"""
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM user_data;')
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            for rows in batch:
+                yield rows
+
+
+
+def batch_processing(batch_size):
+    """ Processes each batch to filter users over the age of 25 """
+    pass
