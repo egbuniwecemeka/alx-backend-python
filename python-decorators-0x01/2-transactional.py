f2f1e7ad89cddf5e43af3c3bf66d22220e1a52c2
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-09-22 00:26:47 +0100

    Transaction Management Decorator

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,26 @@
+import sqlite3
+import functools
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        result = func(conn, *args, **kwargs)
+        conn.close()
+        return result
+    return wrapper
+
+def transactional(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        pass
+
+
+@with_db_connection
+@transactional
+def update_user_email(conn, user_id, new_email):
+    cursor = conn.cursor()
+    cursor.execute('UPDATE users SET email = ? WHERE id = ?', (user_id, new_email,))
+#### Update user's email with automatic transaction handling
+
+update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
