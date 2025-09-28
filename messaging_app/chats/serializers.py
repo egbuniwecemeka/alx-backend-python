a14ce31836b236497e4d9109ded053c0ef5c77a8
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-09-29 00:15:48 +0100

    Create serializers to define the many to many relationships

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from models import User, Message, Conversation
+
+class UserSerializer(serializers.HyperlinkedModeSerializer): # Hyperlinking - Good RESTful design
+    class Meta:
+        model = User
+        fields = ['user_id', 'first_name', 'last_name', 'email',
+                  'password_hash', 'phone_number', 'role', 'created_at'
+                  ]
+
+class MessageSerializer(serializers.HyperlinkedModeSerializer):
+    class Meta:
+        model = Message
+        fields = ['message_id', 'sender_id', 'message_body', 'sent_at']
+
+class ConversationSerializer(serializers.HyperlinkedModeSerializer):
+    class Meta:
+        fields = Conversation
+        name = ['conversation_id', 'participants_id', 'created_at']
