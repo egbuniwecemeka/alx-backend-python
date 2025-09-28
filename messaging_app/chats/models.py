ecabc6eb1657594d263dd2c567b805897a3f957b
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-09-28 02:29:15 +0100

    Project set up

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -4,7 +4,7 @@ import uuid
 
 # Create your models here.
 class User(models.Model):
-    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=100, null=False)
     last_name = models.CharField(max_length=100, null=False)
     email = models.EmailField(unique=True, null=False)
@@ -12,3 +12,12 @@ class User(models.Model):
     phone_number = models.CharField(max_length=20, null=False, blank=True)
     role = models.CharField(max_length=10, choices=[('guest', 'Guest'), ('host', 'Host'), ('admin', 'Admin')], null=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    sender_id = models.ForeignKey('User', on_delete=models.CASCADE, to_field='user_id', related_name='messages')
+    message_body = models.TextField(null=False)
+    sent_at = models.DateTimeField(auto_now_add=True)
