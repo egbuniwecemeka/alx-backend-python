715ccad0e11b274a0d1344810de0ab59cd6c3e09
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-10-01 19:19:41 +0100

    Build api endpoints with views

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -4,9 +4,9 @@ from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
 router.register(r'messages', MessageViewSet)
-router.register(r'conversation', ConversationViewSet)
+router.register(r'conversations', ConversationViewSet)
 
 urlpatterns = [
-    path("api/", include(router.urls)), # Link API views to URLs automatically
-    path("api/api-auth/", include('rest_framework.urls', namespace='rest_framework'))
+    path('api/', include(router.urls)),
+    path('api/api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
