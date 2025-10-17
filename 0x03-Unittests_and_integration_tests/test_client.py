9bbd83a469f8d3304da1bca24544b3576c70ba57
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-10-18 00:43:19 +0100

    More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -27,13 +27,37 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, test_payload)
 
     def test_public_repos_url(self):
-        """ Test for GithubOrgClient._public_repos_url """
+        """ Test that GithubOrgClient._public_repos_url returns the right repos URL"""
+
+        # Patch the org property on GithubOrgClient so it doesn't return the real API
         with patch.object(GithubOrgClient.org, new_callable=PropertyMock) as mock_payload:
+            # Set mock to return a fake organization dictionary
             mock_payload.return_value = {'repos_url': 'http://api.github.com/orgs/testorg/repos'}
             
-            # Result
+            # Create a client instance and access thr _public_repos_url property
             result = GithubOrgClient('testorg')._public_repos_url
 
-            # Test
+            # Check that property returned the expected 'repos_url' value
             self.assertEqual(result, 'http://api.github.com/orgs/testorg/repos')
 
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """ Test GithubOrgClient.public_repos method """
+
+        # Mock the return value of get_json for desired payload
+        mock_get_json.return_value = [{"keys": "values"}, {"keys": "values"}]
+
+        # Patch _public_repos_url property so it doesn't make a real API call
+        with patch.object(GithubOrgClient, 'public_repos_url', new_callable=PropertyMock) as mock_repos_url:
+            # Simulate that the property returns a fake URL
+            mock_repos_url.return_value = 'https://api.github.com/orgs/testorg/repos'
+
+            # Create an instance of the client and call the method being tested
+            result = GithubOrgClient('testorg').public_repos()
+
+
+            # Confirm that the method returns a list of repos name as expected
+            self.assertEqual(result, ['repo1', 'repo2'])
+
+            # Check that get_son was called once with the mocked URL
+            mock_get_json.assert_called_once_with('https://api.github.com/orgs/testorg/repos')
