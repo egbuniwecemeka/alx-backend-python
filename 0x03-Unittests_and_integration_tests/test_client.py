0bc4f5df16cfb2afdab30dd84105eaa9af9e978f
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-10-17 14:12:57 +0100

    Parameterize and patch as decorators

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,20 +2,26 @@
 """ Test module for Github org client """
 
 from unittest import TestCase
-from unittest.mock import patch, PropertyMock
+from unittest.mock import patch, Mock
 from client import GithubOrgClient
 from parameterized import parameterized
 
 class TestGithubOrgClient(TestCase):
-    """Unit tests for GithubOrgClient"""
-
+    """ Tests GithubOrgClient """
+    @parameterized.expand([
+        ('google',),
+        ('abc',),
+    ])
     @patch('client.get_json')
-    def test_public_repos_url(self, mock_get_json):
-        """Test GithubOrgClient._public_repos_url property"""
-        test_payload = {"repos_url": "https://api.github.com/orgs/testorg/repos"}
-        mock_get_json.return_value = test_payload
+    def test_org(self, test_org, mock_get):
+        """ Tests that GithubOrgClient.orrg returns the correct value """
+        test_payload = {'repos_url': f'https://api.github.com/orgs/{test_org}/repos'}
+        mock_get.return_value = test_payload
 
-        client = GithubOrgClient("testorg")
-        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_org:
-            mock_org.return_value = test_payload
-            self.assertEqual(client._public_repos_url, test_payload["repos_url"])
+        client = GithubOrgClient(test_org)
+        result = client.org
+
+        # Test
+        mock_get.assert_called_with(f'https://api.github.com/orgs/{test_org}')
+        self.assertEqual(result, test_payload)
+
